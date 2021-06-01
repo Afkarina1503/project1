034a5c3e1e4306ed7f2a0d8f48b2eaec8e519f73
Afkarina1503 <afkarina1503@gmail.com>
2021-06-01 16:31:58 +0700

    2-06-2021

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import layar1 from './src/layar1';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => layar1);
